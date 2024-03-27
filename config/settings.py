37c6fddeb46c50151166dc034084581b0056e443
Diyora1212 <absalamovadiyora@gmail.com>
2024-03-27 20:24:42 +0500

    spotify

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-^qgg&1hgpikw$o)!r2hi3l)*yls!a)%o++3a^zs0))8^ia&$3-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
